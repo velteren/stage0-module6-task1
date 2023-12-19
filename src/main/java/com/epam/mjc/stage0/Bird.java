77954db80327003a2fee6f06ecd6dceb4041ce62
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-19 12:48:34 +0300

    fix: issues

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -4,6 +4,9 @@ public class Bird extends Animal {
     public Bird (String color, int numberOfPaws, boolean hasFur) {
         super("blue", 2, false);
     }
+    public Bird() {
+        super("blue", 2, false);
+    }
 
     @Override
     String getDescription() {
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -4,4 +4,7 @@ public class Dog extends Animal {
     public Dog (String color, int numberOfPaws, boolean hasFur) {
         super("brown", 4, true);
     }
+    public Dog() {
+        super("brown", 4, true);
+    }
 }
